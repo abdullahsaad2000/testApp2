9fbd05b801b734448800c4564c16b26cbefd3372
abdullahsaad2000 <harthi1127@gmail.com>
2024-02-13 19:49:32 +0300

    alllllll

---
+++ my-app/src/components/fm1.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './fm1.css'
+function fm1() {
+  return (
+    <div>
+      <p className='h'>jhiiii</p>
+      
+    </div>
+  )
+}
+
+export default fm1
---
+++ my-app/src/mythis.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function mythis() {
+  return (
+    <div>mythis</div>
+  )
+}
+
+export default mythis
